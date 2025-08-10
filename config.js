b7bb2f91cfdf9baa665b890be0acac0d4d02dfa7
yaakov2731 <yaakovrubi@gmail.com>
2025-08-10 19:07:21 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+window.CONFIG={APPS_SCRIPT_URL:"https://script.google.com/macros/s/AKfycbwAFdsy2GdJ2dUnuYYMJUprdVaT3cefp5LyMYl_iRlbQCSO1krDbVV24AbWeiiw9CnRNg/exec",SHEET_ID:"1Syj1DXWyrm-9CdcNFGXLidLHF-MzuF0UfbCd-T2PhiE"};
+window.build=(a,p={})=>((window.CONFIG.APPS_SCRIPT_URL||"").replace(/\/$/,""))+"?"+new URLSearchParams(Object.assign({action:a,sheetId:window.CONFIG.SHEET_ID},p)).toString();
